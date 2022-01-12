768e1d6cdfc8e461b8329393f0e56394ef025ba0
Ngoc Ngo <ngocnv371@gmail.com>
2022-01-12 16:08:33 +0700

    format table

--- client/src/pages/QuizzesPage.tsx
+++ client/src/pages/QuizzesPage.tsx
@@ -1,3 +1,19 @@
+import { Container, Paper } from "@mui/material";
+
+import QuizzesTable from "../containers/QuizzesTable";
+
 export default function QuizzesPage() {
-  return <h3>Managing stuff here</h3>;
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Paper
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <QuizzesTable />
+      </Paper>
+    </Container>
+  );
 }
