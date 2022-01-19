716829b7dbec3e199a3b780bf2ccd5d1ab8828be
Ngoc Ngo <ngocnv371@gmail.com>
2022-01-19 17:33:43 +0700

    correct nav

--- client/src/components/app-bar.vue
+++ client/src/components/app-bar.vue
@@ -19,8 +19,6 @@ export default {
     <v-app-bar-nav-icon @click.stop="toggle"></v-app-bar-nav-icon>
 
     <v-toolbar-title>{{ $route.name }}</v-toolbar-title>
-    <v-divider class="mx-4" inset vertical></v-divider>
-    <router-link to="/questions"></router-link>
     <v-spacer />
     <slot>
       <v-avatar>
--- client/src/components/nav-drawer.vue
+++ client/src/components/nav-drawer.vue
@@ -41,6 +41,9 @@ export default {
     <v-divider></v-divider>
     <v-list nav dense>
       <v-list-item-group active-class="deep-purple--text text--accent-4">
+        <v-list-item to="/">
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
         <v-list-item to="/questions">
           <v-list-item-title>Questions</v-list-item-title>
         </v-list-item>
